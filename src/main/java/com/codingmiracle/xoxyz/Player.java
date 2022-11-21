1cc8dc1874809a4a17de469df5aea4f04345640a
David <dav.gruber@htlstp.at>
2022-11-21 09:56:43 +0100

    add: rest-api for player

--- src/main/java/com/codingmiracle/xoxyz/Player.java
+++ src/main/java/com/codingmiracle/xoxyz/Player.java
@@ -3,6 +3,7 @@ package com.codingmiracle.xoxyz;
 public class Player {
     private long id;
     private String name;
+    private static long idgen = 0;
 
     public Player(long id, String name) {
         this.id = id;
@@ -10,7 +11,21 @@ public class Player {
     }
 
     public Player() {
+        this.name = null; this.id = generateId();
+    }
+
+    public Player(String name) {
+        this.name = name;
+        this.id = generateId();
+    }
+
+    public Player(long id) {
+        this.id = id;
         this.name = null;
+    }
+
+    public static long generateId() {
+        return idgen++;
     }
 
     public long getId() {
@@ -29,7 +44,22 @@ public class Player {
         this.name = name;
     }
 
-    public Player(String name) {
-        this.name = name;
+    @Override
+    public boolean equals(Object obj) {
+        if(obj.getClass() == Player.class) {
+            Player playerObj = (Player)obj;
+            return playerObj.getId() == this.id;
+        }
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
--- src/main/java/com/codingmiracle/xoxyz/PlayerRestService.java
+++ src/main/java/com/codingmiracle/xoxyz/PlayerRestService.java
@@ -1,26 +1,40 @@
 package com.codingmiracle.xoxyz;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Path("/player")
+@Produces(MediaType.APPLICATION_JSON)
 public class PlayerRestService {
-    private List<Player> playerList = new ArrayList<Player>();
-    @Path("/new")
+
+    private List<Player> playerList = new ArrayList<>();
+
+    @Path("/{PlayerName}")
     @POST
-    public void createPlayer(String name) {
-        playerList.add(new Player(name));
+    public void createPlayer(@PathParam("PlayerName") String name) {
+        Player player = new Player(name);
+        playerList.add(player);
+    }
+
+    @Path("/{PlayerId}")
+    @DELETE
+    public void deletePlayer(@PathParam("PlayerId") long playerId) {
+        playerList.remove(new Player(playerId));
     }
 
     @Path("/{PlayerId}")
     @GET
     public Player getPlayer(@PathParam("PlayerId") long playerId) {
-        return new Player(playerId, "mark");
+        return playerList.get(playerList.indexOf(new Player(playerId)));
+    }
+
+    @Path("/list")
+    @POST
+    public List<Player> getPlayerList() {
+        return playerList;
     }
 
 
